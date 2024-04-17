0f46b3bb96272a5d7e4280527f6f695a998e1520
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-04-17 23:33:34 +0300

    Update SumOfGCP_LCM.java

--- Softuniada 2022/SumOfGCP_LCM.java
+++ Softuniada 2022/SumOfGCP_LCM.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
 public class SumOfGCP_LCM {
-    private static final Scanner scanner = new Scanner(System.in);
-    private static int firstNum;
-    private static int secondNum;
     private static int biggerDivider;
 
     public static void main(String[] args) {
-        firstNum = Integer.parseInt(scanner.nextLine());
-        secondNum = Integer.parseInt(scanner.nextLine());
-        findBiggerDivider();
-        System.out.println(biggerDivider + findLeastCommonMultiple());
+        Scanner scanner = new Scanner(System.in);
+        int firstNum = Integer.parseInt(scanner.nextLine());
+        int secondNum = Integer.parseInt(scanner.nextLine());
+        findBiggerDivider(firstNum, secondNum);
+        System.out.println(biggerDivider + findLeastCommonMultiple(firstNum, secondNum));
     }
 
-    private static int findLeastCommonMultiple() {
+    private static int findLeastCommonMultiple(int firstNum, int secondNum) {
         for (int i = 1; i < 1000000; i++) {
             if ((i / firstNum * firstNum == i) && (i / secondNum * secondNum == i)) {
                 return i;
@@ -22,7 +20,7 @@ public class SumOfGCP_LCM {
         return 0;
     }
 
-    private static void findBiggerDivider() {
+    private static void findBiggerDivider(int firstNum, int secondNum) {
         for (int i = 1; i < 1000000; i++) {
             if (firstNum % i == 0 && secondNum % i == 0) {
                 biggerDivider = i;
