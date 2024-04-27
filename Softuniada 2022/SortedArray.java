a02779a0af8a346a874786b68cbf221345a2bd7b
Antoan Metodiev <Boksnem@abv.bg>
2024-04-27 23:58:51 +0300

    add solution

---
+++ Softuniada 2022/SortedArray.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int currentNum = 0;
+        String flag = "odd";
+
+        for (int i = 0; i < n; i++) {
+            currentNum = numbers[i];
+
+            if ((flag.equals("odd") && (i + 1) < n) && !(currentNum >= numbers[i + 1])) {
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = currentNum;
+            } else if ((flag.equals("even") && (i + 1) < n) && !(currentNum <= numbers[i + 1])) {
+                numbers[i] = numbers[i + 1];
+                numbers[i + 1] = currentNum;
+            }
+
+            if (flag.equals("odd")) {
+                flag = "even";
+                continue;
+            }
+            flag = "odd";
+        }
+
+        System.out.println(String.join(" ", Arrays.toString(numbers).replaceAll("[\\[\\],]", "")));
+    }
+
+}
+
+
